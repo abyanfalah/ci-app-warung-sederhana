73b40787cb536396fa46a259d1b5e05447a9c6df
abyanfalah <abyanf@protonmail>
2022-06-12 21:16:06 +0700

    barang deletion works

--- application/views/admin/barang/delete.php
+++ application/views/admin/barang/delete.php
@@ -1,41 +1,41 @@
 
 <div class="card shadow mx-auto" style="width: 500px">
 	<div class="card-header bg-danger text-white">
-		<strong>Hapus user dengan informasi berikut?</strong>
+		<strong>Hapus barang dengan informasi berikut?</strong>
 	</div>
 	<div class="card-body">
 			
 			<div class="row">
 				<div class="col-3">ID</div>
 				<div class="col-1">:</div>
-				<div class="col"><?php echo ucwords($user->id);?></div>
+				<div class="col"><?php echo ucwords($barang->id);?></div>
 			</div>
 
 			<div class="row">
 				<div class="col-3">Nama</div>
 				<div class="col-1">:</div>
-				<div class="col"><?php echo ucwords($user->nama);?></div>
+				<div class="col"><?php echo ucwords($barang->nama);?></div>
 			</div>
 
 			<div class="row">
-				<div class="col-3">Alamat</div>
+				<div class="col-3">Jenis</div>
 				<div class="col-1">:</div>
-				<div class="col"><?php echo $user->alamat; ?></div>
+				<div class="col"><?php echo $barang->jenis; ?></div>
 			</div>
 
 			<div class="row">
-				<div class="col-3">Akses</div>
+				<div class="col-3">Harga</div>
 				<div class="col-1">:</div>
-				<div class="col"><?php echo ucwords($user->akses);?></div>
+				<div class="col"><?php echo ucwords($barang->harga);?></div>
 			</div>
 	</div>
 
 	<div class="card-footer border-0 bg-white text-right">
-		<a href="<?php echo base_url('user') ?>" class="btn btn-secondary">Batal</a>
+		<a href="<?php echo base_url('barang') ?>" class="btn btn-secondary">Batal</a>
 		<button class="btn btn-danger" id="btnProceedDelete">
 			Ya, hapus
 		</button>
 	</div>
 </div>
 
-<script type="text/javascript" src="<?php echo base_url('assets/script/user_delete.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/script/barang_delete.js') ?>"></script>
---
+++ assets/script/barang_delete.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("#btnProceedDelete").click(function(){
+		$.ajax({
+			url: "/api/barang/delete",
+			type: "POST",
+			success: function(res){
+				if(res.status == 200){
+					localStorage.setItem('barang_id', res.barang_id)
+					localStorage.setItem('alertMessage', 'delete')
+					window.location.replace('/barang')
+				}
+				// console.log(res)
+			}
+		})
+	})
+})

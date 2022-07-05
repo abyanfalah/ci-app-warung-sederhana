040e8fc59769081892f92fadbddb70e1996bb992
abyanfalah <abyanf@protonmail>
2022-07-05 19:17:25 +0700

    update dashboard view

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,98 +1,14 @@
-<div class="row mb-3 d-flex justify-content-center">
-
-	<!-- Penjualan hari ini -->
-	<div class="col">
-		<div class="card shadow-sm bg-primary text-white">
-			<div class="card-header text-primary">
-				<strong>Penjualan hari ini</strong>
-			</div>
-			<div class="card-body">	
-				<div class="row">
-					<div class="col">
-						<span class="display-4">
-							<i class="fas fa-fw fa-database"></i>
-						</span>
-					</div>
-					<div class="col text-right ">
-						<h1>30</h1>
-					</div>
-				</div>
-			</div>
+<?php 
+	if ($this->session->flashdata('just_logged_in')) {
+	// if(true){
+?>
+		<div class="alert alert-success" id="welcomeAlert">
+			Selamat datang <?php echo ucwords($this->session->userdata('nama')); ?>
 		</div>
-	</div>
+<?php
+	}
 
-	<!-- Omset -->
-	<div class="col">
-		<div class="card shadow-sm bg-success text-white">
-			<div class="card-header text-success">
-				<strong>Omset</strong>
-			</div>
-			<div class="card-body">	
-				<div class="row">
-					<div class="col">
-						<span class="display-4">
-							<i class="fas fa-fw fa-dollar-sign"></i>
-						</span>
-					</div>
-					<div class="col text-right ">
-						<h1>576,000</h1>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<!-- Penjualan hari ini -->
-	<div class="col">
-		<div class="card shadow-sm bg-danger text-white">
-			<div class="card-header text-danger">
-				<strong>Penjualan hari ini</strong>
-			</div>
-			<div class="card-body">	
-				<div class="row">
-					<div class="col">
-						<span class="display-4">
-							<i class="fas fa-fw fa-database"></i>
-						</span>
-					</div>
-					<div class="col text-right ">
-						<h1>30</h1>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	
-</div>
+ ?>
 
 
-<div class="row">
-	<div class="col-6">
-		<div class="card shadow-sm">
-			<div class="card-header bg-warning" style="color:black;">
-				<!-- <i class="fas fa-fw fa-chart"></i> -->
-				<strong>Riwayat Transaksi</strong>
-			</div>
-			<div class="card-body">
-				<table class="table table-borderless">
-					<thead class="bg-light">
-						<th>#</th>
-						<th>Pembeli</th>
-						<th>Total</th>
-						<th>Kasir</th>
-					</thead>
-
-					<tbody>
-						<tr>
-							<td>220518001</td>
-							<td>Ujang</td>
-							<td>45000</td>
-							<td>Asep</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</div>
+<script type="text/javascript" src="<?php echo base_url('assets/script/dashboard.js') ?>"></script>

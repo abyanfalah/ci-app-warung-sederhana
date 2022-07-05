618d1122f4a888a2982ba4fa262e00eb6d907f5b
abyanfalah <abyanf@protonmail>
2022-07-05 19:21:14 +0700

    created admin/barang/supply view

---
+++ application/views/admin/barang/supply.php
@@ -0,0 +1,83 @@
+
+
+<!-- col pilih supplier dan display data supplier -->
+<div class="row">
+	<div class="col">
+		<strong>Supplier</strong>
+		<button class="btn btn-primary" data-toggle="modal" data-target="#modalPilihSupplier">Pilih supplier</button>
+
+		<div id="supplierDetail" style="display:none;" class="col-6">
+			<div class="row">
+				<div class="col">ID</div>
+				<div class="col" id="idSupplier"></div>
+			</div>
+
+			<div class="row">
+				<div class="col">Nama</div>
+				<div class="col" id="namaSupplier"></div>
+			</div>
+
+			<div class="row">
+				<div class="col">Telpon</div>
+				<div class="col" id="telponSupplier"></div>
+			</div>
+
+			<div class="row">
+				<div class="col">Asal</div>
+				<div class="col" id="asalSupplier"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- col pilih barang untuk update stok -->
+<div class="row" id="sectionPilihBarang" style="display:none;">
+	<div class="col">
+		section Pilih barang
+	</div>
+</div>
+
+
+
+
+
+
+
+
+<!-- modal pilih supplier -->
+<!-- modal checkout -->
+<div id="modalPilihSupplier" class="modal fade" data-backdrop="static">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div class="row">
+					<div class="col">
+						<h3>Pilih supplier</h3>
+					</div>
+					<div class="col text-right">
+						<button data-dismiss="modal" class="btn btn-danger">Batal</button>		
+					</div>
+				</div>				
+				<table class="table table-striped" id="tableListSupplier" width="100%">
+								<thead>
+									<tr>
+										<th class="col-1">#</th>
+										<th>ID</th>
+										<th>Nama</th>
+										<th>Asal</th>
+										<th colspan="2">Telpon</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<!-- diisi dengan js -->
+								</tbody>
+					</table>
+			</div>			
+		</div>
+
+	</div>
+</div>
+
+
+<script type="text/javascript" src="<?php echo base_url('assets/script/barang_supply.js') ?>"></script>

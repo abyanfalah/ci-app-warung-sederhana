fe27191e2b4d22e5a482c7b82b152f9137dc0070
abyanfalah <abyanf@protonmail>
2022-07-05 19:19:29 +0700

    update dashboard controller

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -15,22 +15,39 @@ class Dashboard extends CI_Controller {
 		}
 
 		$this->load->model('user_model');
+		$this->load->model('barang_model');
+		$this->load->model('transaksi_model');
+
 		$this->access = $this->session->userdata('akses');
-		$this->session->unset_userdata('edit');
-		$this->session->unset_userdata('delete');
 	}
 
 	public function index()
 	{
 		$data = [
-			'title' => 'Dashboard',
-			'user'  => $this->user_model->get_all(),
+			'title'              => 'Dashboard',
+			'user'               => $this->user_model->get_all(),
+			'barang'             => $this->barang_model->get_all(),
+			'transaksi'          => $this->transaksi_model->get_all(),
+			'transaksi_hari_ini' => $this->transaksi_model->get_by_date()
 		];
 
 		$this->load->view($this->access."/_partials/header", $data);
 		$this->load->view($this->access."/_partials/sidebar", $data);
 		$this->load->view($this->access."/dashboard", $data);
 		$this->load->view($this->access."/_partials/footer");
+	}
+
+	public function test()
+	{
+		$a = $this->transaksi_model->get_by_date('20220620');
+		// $a = $this->transaksi_model->get_all();
+		// $a = $a->num_rows();
+		$a = $a->result_array();
+		$res = [];
+		$res = $a;
+
+		header('Content-Type: application/json');
+		echo json_encode($res, JSON_PRETTY_PRINT);
 	}
 
 }
